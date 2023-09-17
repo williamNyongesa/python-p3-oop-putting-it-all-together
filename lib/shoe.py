eb82aa01f0b80a780dbd87946044927ba767ca4b
William Nyongesa <marakawilliam@gmail.com>
2023-09-17 09:46:08 +0300

    complete book.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,size) -> None:
+        self.size = size
+        
+    def change_size(self,new_size):
+        self.size = new_size
+
+bata_shoe = Shoe(42)
+print(bata_shoe.size)
